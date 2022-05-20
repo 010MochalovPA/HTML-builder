ed1b0d8f8ca5d0a831622cf93cb790dbdb50a754
Павел Мочалов <010mochalovpa@gmail.com>
2022-05-20 21:13:32 +0300

    refactor: change variable

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+const ReadStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 let data = '';
-stream.on('data', chunk => data += chunk);
-stream.on('end', () => console.log(data));
+ReadStream.on('data', chunk => data += chunk);
+ReadStream.on('end', () => console.log(data));
