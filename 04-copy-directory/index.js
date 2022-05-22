eb3939a1b56b090695f59500120c8c74c6d8ea2a
Павел Мочалов <010mochalovpa@gmail.com>
2022-05-22 10:29:18 +0300

    feat: complite 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
 
+const fs = require('fs');
+const path = require('path');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) throw err;
+  console.log('Директория files-copy создана!')
+});
+
+fs.readdir(path.join(__dirname, 'files-copy'),(e,files)=>{
+  files.forEach(file => {
+    fs.unlink(path.join(__dirname, 'files-copy' , file), (err) => {
+      if (err) throw err;
+    });
+  })
+});
+
+fs.readdir(path.join(__dirname, 'files'),(e,files)=>{
+  files.forEach(file => {
+    fs.copyFile(path.join(__dirname, 'files' , file), path.join(__dirname, 'files-copy' , file), (err) => {
+      if (err) throw err;
+      console.log(`${file} скопирован`);
+    });
+  })
+});
+
