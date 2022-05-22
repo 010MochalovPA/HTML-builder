53757009e8e8ded6e613b210cc0296e8abf0adc4
Павел Мочалов <010mochalovpa@gmail.com>
2022-05-22 10:31:57 +0300

    fix: fix import

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
-const { stdin, stdout } = process;
+const process = require('process');
 const path = require('path');
 
 
 const WriteStream = fs.createWriteStream(path.join(__dirname, 'output.txt'));
 
-stdout.write('Введите текст:\n');
-stdin.on('data', data => {
+process.stdout.write('Введите текст:\n');
+process.stdin.on('data', data => {
   if (data.toString().trim() === 'exit') process.exit();
   WriteStream.write(data);
 });
 
 process.on('SIGINT', () => process.exit());
-process.on('exit', () => stdout.write('Удачи в изучении Node.js!'));
+process.on('exit', () => process.stdout.write('Удачи в изучении Node.js!'));
