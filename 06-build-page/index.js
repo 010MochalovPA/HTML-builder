f37e460eb7a105a1b2c541931efa2932b7082bc0
Павел Мочалов <010mochalovpa@gmail.com>
2022-05-23 17:57:58 +0300

    feat: add truescorrect indentation

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,20 +6,20 @@ fs.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }, (err) => {
     if (err) throw err;
 });
 
-const WriteStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
 const ReadStream = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
 let template = '';
 ReadStream.on('data', chunk => template += chunk);
 ReadStream.on('end', ()=> {
     fs.readdir(path.join(__dirname, 'components'),{withFileTypes: true},(err,files)=>{
         files.forEach(file => {
-            let componentData = ''
+            let componentData = '';
             const component = path.basename(file.name, path.extname(file.name)).toString();
             const ReadStreamComponent = fs.createReadStream(path.join(__dirname,'components' ,`${component}.html`), 'utf-8');
+            const indexComponent = template.substring(0,template.indexOf('{{' + component+ '}}')).length - template.substring(0,template.indexOf('{{' + component+ '}}')).lastIndexOf('\n') -1;
             ReadStreamComponent.on('data', chunk => componentData += chunk);
             ReadStreamComponent.on('end', () => {
                 const WriteStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'index.html'));
-                template = template.replace('{{' + component + '}}', componentData.toString() );
+                template = template.replace(' '.repeat(indexComponent) +'{{' + component + '}}', componentData.split('\n').map(line => ' '.repeat(indexComponent) + line).join('\n') );
                 WriteStream.write(template);
             });
         
