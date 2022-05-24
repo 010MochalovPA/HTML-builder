555b1bda96d20ed4266f395122ad1c11c767bc59
Mochalov Pavel <010mochalovpa@gmail.com>
2022-05-24 10:29:51 +0300

    refactor: fix scopes

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,22 +6,20 @@ fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
   if (err) throw err;
   console.log('Директория files-copy создана!');
 
-fs.readdir(path.join(__dirname, 'files-copy'),(e,files)=>{
-  files.forEach(file => {
-    fs.unlink(path.join(__dirname, 'files-copy' , file), (err) => {
-      if (err) throw err;
+  fs.readdir(path.join(__dirname, 'files-copy'),(e,files)=>{
+    files.forEach(file => {
+      fs.unlink(path.join(__dirname, 'files-copy' , file), (err) => {
+        if (err) throw err;
+      });
+    });
+
+    fs.readdir(path.join(__dirname, 'files'),(e,files)=>{
+      files.forEach(file => {
+        fs.copyFile(path.join(__dirname, 'files' , file), path.join(__dirname, 'files-copy' , file), (err) => {
+          if (err) throw err;
+          console.log(`${file} скопирован`);
+        });
+      });
     });
   });
-});
-
-fs.readdir(path.join(__dirname, 'files'),(e,files)=>{
-  files.forEach(file => {
-    fs.copyFile(path.join(__dirname, 'files' , file), path.join(__dirname, 'files-copy' , file), (err) => {
-      if (err) throw err;
-      console.log(`${file} скопирован`);
-    });
-  });
-});
-
-
 });
