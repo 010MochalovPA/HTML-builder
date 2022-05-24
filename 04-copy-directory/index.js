af76bb2340661e8c7d56f010de7823d920e21593
Mochalov Pavel <010mochalovpa@gmail.com>
2022-05-24 10:35:24 +0300

    refacrot: add function copyDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,25 +1,28 @@
 
 const fs = require('fs');
 const path = require('path');
-
-fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
-  if (err) throw err;
-  console.log('Директория files-copy создана!');
-
-  fs.readdir(path.join(__dirname, 'files-copy'),(e,files)=>{
-    files.forEach(file => {
-      fs.unlink(path.join(__dirname, 'files-copy' , file), (err) => {
-        if (err) throw err;
-      });
-    });
-
-    fs.readdir(path.join(__dirname, 'files'),(e,files)=>{
+function copyDir(){
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) throw err;
+    console.log('Директория files-copy создана!');
+  
+    fs.readdir(path.join(__dirname, 'files-copy'),(e,files)=>{
       files.forEach(file => {
-        fs.copyFile(path.join(__dirname, 'files' , file), path.join(__dirname, 'files-copy' , file), (err) => {
+        fs.unlink(path.join(__dirname, 'files-copy' , file), (err) => {
           if (err) throw err;
-          console.log(`${file} скопирован`);
+        });
+      });
+  
+      fs.readdir(path.join(__dirname, 'files'),(e,files)=>{
+        files.forEach(file => {
+          fs.copyFile(path.join(__dirname, 'files' , file), path.join(__dirname, 'files-copy' , file), (err) => {
+            if (err) throw err;
+            console.log(`${file} скопирован`);
+          });
         });
       });
     });
   });
-});
+}
+
+copyDir();
